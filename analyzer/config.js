0c552bc29d44bbc607e4b1fcd700b3cabcc0e3ab
hbaughman <hbaughman741@gmail.com>
2015-01-24 12:07:48 -0500

    Refactor homeRow, keyboard, layouts to config.js

---
+++ analyzer/config.js
@@ -0,0 +1,518 @@
+// dl is the devLayer namespace
+dl.config = {
+
+  "xmodToSymbol": {
+    "NoSymbol": "",
+    "Escape": "Esc",
+    "exclam": "!",
+    "at": "@",
+    "numbersign": "#",
+    "dollar": "$",
+    "percent": "%",
+    "asciicircum": "^",
+    "ampersand": "&",
+    "asterisk": "*",
+    "parenleft": "(",
+    "parenright": ")",
+    "minus": "-",
+    "underscore": "_",
+    "equal": "=",
+    "plus": "+",
+    // "BackSpace": "BackSpace",
+    // "Tab": "Tab",
+    "bracketleft": "[",
+    "bracketright": "]",
+    "braceleft": "{",
+    "braceright": "}",
+    "Return": "Enter",
+    "semicolon": ";",
+    "colon": ":",
+    "apostrophe": "'",
+    "quotedbl": "\"",
+    "grave": "`",
+    "asciitilde": "~",
+    "backslash": "\\",
+    "bar": "|",
+    "comma": ",",
+    "less": "<",
+    "period": ".",
+    "greater": ">",
+    "slash": "/",
+    "question": "?",
+    "space": "Space",
+    "Caps_Lock": "CapsLock"
+    // "Home": "Home",
+    "Up": "PgUp",
+    "Prior": "PgDn"
+    // "End": "End"
+  },
+
+  "symbolToXmod": {
+    "": "NoSymbol",
+    "Esc": "Escape",
+    "!": "exclam",
+    "@": "at",
+    "#": "numbersign",
+    "$": "dollar",
+    "%": "percent",
+    "^": "asciicircum",
+    "&": "ampersand",
+    "*": "asterisk",
+    "(": "parenleft",
+    ")": "parenright",
+    "-": "minus",
+    "_": "underscore",
+    "=": "equal",
+    "+": "plus",
+    "[": "bracketleft",
+    "]": "bracketright",
+    "{": "braceleft",
+    "}": "braceright",
+    "Enter": "Return",
+    ";": "semicolon",
+    ":": "colon",
+    "'": "apostrophe",
+    "\"": "quotedbl",
+    "`": "grave",
+    "~": "asciitilde",
+    "\\": "backslash",
+    "|": "bar",
+    ",": "comma",
+    "<": "less",
+    ".": "period",
+    ">": "greater",
+    "/": "slash",
+    "?": "question",
+    "Space": "space",
+    "CapsLock": "Caps_Lock"
+    "PgUp": "Up",
+    "PgDn": "Prior"
+  },
+
+  "minimumKeycode": 10,
+  "maximumKeycode": 66,
+
+  "inputPriority": ["altGr", "altGrShift", "noMod", "shift"],
+  "easyPriority": ["noMod", "altGr", "shift", "altGrShift"],
+  "modifiers": {
+    "noMod": { "altGr": false, "shift": false },
+    "shift": { "altGr": false, "shift": true },
+    "altGr": { "altGr": true, "shift": false },
+    "altGrShift": { "altGr": true, "shift": true }
+  },
+
+  "homerow": {
+    "leftPinkie": 38,
+    "leftRing": 39,
+    "leftMiddle": 40,
+    "leftIndex": 41,
+    "leftThumb": 65,
+    "rightThumb": 108,
+    "rightIndex": 44,
+    "rightMiddle": 45,
+    "rightRing": 46,
+    "rightPinkie": 47
+  },
+
+  "keyboard": {
+    // qwerty: 1
+    "10": { 
+      "hand": "left",
+      "finger": "leftPinkie",
+      "x": 1,
+      "y": 0
+    }, 
+    // qwerty: 2  
+    "11": { 
+      "hand": "left",
+      "finger": "leftRing",
+      "x": 2,
+      "y": 0
+    }, 
+    // qwerty: 3  
+    "12": { 
+      "hand": "left",
+      "finger": "leftMiddle",
+      "x": 3,
+      "y": 0
+    }, 
+    // qwerty: 4  
+    "13": { 
+      "hand": "left",
+      "finger": "leftIndex",
+      "x": 4,
+      "y": 0
+    }, 
+    // qwerty: 5  
+    "14": { 
+      "hand": "left",
+      "finger": "leftIndex",
+      "x": 5,
+      "y": 0
+    }, 
+    // qwerty: 6  
+    "15": { 
+      "hand": "right",
+      "finger": "rightIndex",
+      "x": 6,
+      "y": 0
+    }, 
+    // qwerty: 7  
+    "16": { 
+      "hand": "right",
+      "finger": "rightIndex",
+      "x": 7,
+      "y": 0
+    }, 
+    // qwerty: 8  
+    "17": { 
+      "hand": "right",
+      "finger": "rightMiddle",
+      "x": 8,
+      "y": 0
+    }, 
+    // qwerty: 9  
+    "18": { 
+      "hand": "right",
+      "finger": "rightRing",
+      "x": 9,
+      "y": 0
+    }, 
+    // qwerty: 0  
+    "19": { 
+      "hand": "right",
+      "finger": "rightPinkie",
+      "x": 10,
+      "y": 0
+    }, 
+    // qwerty: minus  
+    "20": { 
+      "hand": "right",
+      "finger": "rightPinkie",
+      "x": 11,
+      "y": 0
+    }, 
+    // qwerty: equal  
+    "21": { 
+      "hand": "right",
+      "finger": "rightPinkie",
+      "x": 12,
+      "y": 0
+    }, 
+    // qwerty: BackSpace  
+    "22": { 
+      "hand": "right",
+      "finger": "rightPinkie",
+      "x": 13,
+      "y": 0
+    }, 
+    // qwerty: Tab  
+    "23": { 
+      "hand": "left",
+      "finger": "leftPinkie",
+      "x": 0.5,
+      "y": 1
+    }, 
+    // qwerty: q  
+    "24": { 
+      "hand": "left",
+      "finger": "leftPinkie",
+      "x": 1.5,
+      "y": 1
+    }, 
+    // qwerty: w  
+    "25": { 
+      "hand": "left",
+      "finger": "leftRing",
+      "x": 2.5,
+      "y": 1
+    }, 
+    // qwerty: e  
+    "26": { 
+      "hand": "left",
+      "finger": "leftMiddle",
+      "x": 3.5,
+      "y": 1
+    }, 
+    // qwerty: r  
+    "27": { 
+      "hand": "left",
+      "finger": "leftIndex",
+      "x": 4.5,
+      "y": 1
+    }, 
+    // qwerty: t  
+    "28": { 
+      "hand": "left",
+      "finger": "leftIndex",
+      "x": 5.5,
+      "y": 1
+    }, 
+    // qwerty: y  
+    "29": { 
+      "hand": "right",
+      "finger": "rightIndex",
+      "x": 6.5,
+      "y": 1
+    }, 
+    // qwerty: u  
+    "30": { 
+      "hand": "right",
+      "finger": "rightIndex",
+      "x": 7.5,
+      "y": 1
+    }, 
+    // qwerty: i  
+    "31": { 
+      "hand": "right",
+      "finger": "rightMiddle",
+      "x": 8.5,
+      "y": 1
+    }, 
+    // qwerty: o  
+    "32": { 
+      "hand": "right",
+      "finger": "rightRing",
+      "x": 9.5,
+      "y": 1
+    }, 
+    // qwerty: p  
+    "33": { 
+      "hand": "right",
+      "finger": "rightPinkie",
+      "x": 10.5,
+      "y": 1
+    }, 
+    // qwerty: bracketleft  
+    "34": { 
+      "hand": "right",
+      "finger": "rightPinkie",
+      "x": 11.5,
+      "y": 1
+    }, 
+    // qwerty: bracketright  
+    "35": { 
+      "hand": "right",
+      "finger": "rightPinkie",
+      "x": 12.5,
+      "y": 1
+    }, 
+    // qwerty: Return  
+    "36": { 
+      "hand": "right",
+      "finger": "rightPinkie",
+      "x": 12.75,
+      "y": 2
+    }, 
+    // qwerty: Control_L  
+    "37": { 
+      "hand": "left",
+      "finger": "leftPinkie",
+      "x": 0,
+      "y": 4
+    }, 
+    // qwerty: a  
+    "38": { 
+      "hand": "left",
+      "finger": "leftPinkie",
+      "x": 1.75,
+      "y": 2
+    }, 
+    // qwerty: s  
+    "39": { 
+      "hand": "left",
+      "finger": "leftRing",
+      "x": 2.75,
+      "y": 2
+    }, 
+    // qwerty: d  
+    "40": { 
+      "hand": "left",
+      "finger": "leftMiddle",
+      "x": 3.75,
+      "y": 2
+    }, 
+    // qwerty: f  
+    "41": { 
+      "hand": "left",
+      "finger": "leftIndex",
+      "x": 4.75,
+      "y": 2
+    }, 
+    // qwerty: g  
+    "42": { 
+      "hand": "left",
+      "finger": "leftIndex",
+      "x": 5.75,
+      "y": 2
+    }, 
+    // qwerty: h  
+    "43": { 
+      "hand": "right",
+      "finger": "rightIndex",
+      "x": 6.75,
+      "y": 2
+    }, 
+    // qwerty: j  
+    "44": { 
+      "hand": "right",
+      "finger": "rightIndex",
+      "x": 7.75,
+      "y": 2
+    }, 
+    // qwerty: k  
+    "45": { 
+      "hand": "right",
+      "finger": "rightMiddle",
+      "x": 8.75,
+      "y": 2
+    }, 
+    // qwerty: l  
+    "46": { 
+      "hand": "right",
+      "finger": "rightRing",
+      "x": 9.75,
+      "y": 2
+    }, 
+    // qwerty: semicolon  
+    "47": { 
+      "hand": "right",
+      "finger": "rightPinkie",
+      "x": 10.75,
+      "y": 2
+    }, 
+    // qwerty: apostrophe  
+    "48": { 
+      "hand": "right",
+      "finger": "rightPinkie",
+      "x": 11.75,
+      "y": 2
+    }, 
+    // qwerty: grave  
+    "49": { 
+      "hand": "left",
+      "finger": "leftPinkie",
+      "x": 0,
+      "y": 0
+    }, 
+    // qwerty: Shift_L  
+    "50": { 
+      "hand": "left",
+      "finger": "leftPinkie",
+      "x": 1.25,
+      "y": 3
+    }, 
+    // qwerty: backslash  
+    "51": { 
+      "hand": "right",
+      "finger": "rightPinkie",
+      "x": 13.5,
+      "y": 1
+    }, 
+    // qwerty: z  
+    "52": { 
+      "hand": "left",
+      "finger": "leftPinkie",
+      "x": 2.25,
+      "y": 3
+    }, 
+    // qwerty: x  
+    "53": { 
+      "hand": "left",
+      "finger": "leftRing",
+      "x": 3.25,
+      "y": 3
+    }, 
+    // qwerty: c  
+    "54": { 
+      "hand": "left",
+      "finger": "leftMiddle",
+      "x": 4.25,
+      "y": 3
+    }, 
+    // qwerty: v  
+    "55": { 
+      "hand": "left",
+      "finger": "leftIndex",
+      "x": 5.25,
+      "y": 3
+    }, 
+    // qwerty: b  
+    "56": { 
+      "hand": "left",
+      "finger": "leftIndex",
+      "x": 6.25,
+      "y": 3
+    }, 
+    // qwerty: n  
+    "57": { 
+      "hand": "right",
+      "finger": "rightIndex",
+      "x": 7.25,
+      "y": 3
+    }, 
+    // qwerty: m  
+    "58": { 
+      "hand": "right",
+      "finger": "rightIndex",
+      "x": 8.25,
+      "y": 3
+    }, 
+    // qwerty: comma  
+    "59": { 
+      "hand": "right",
+      "finger": "rightMiddle",
+      "x": 9.25,
+      "y": 3
+    }, 
+    // qwerty: period  
+    "60": { 
+      "hand": "right",
+      "finger": "rightRing",
+      "x": 10.25,
+      "y": 3
+    }, 
+    // qwerty: slash  
+    "61": { 
+      "hand": "right",
+      "finger": "rightPinkie",
+      "x": 11.25,
+      "y": 3
+    }, 
+    // qwerty: Shift_R  
+    "62": { 
+      "hand": "right",
+      "finger": "rightPinkie",
+      "x": 12.25,
+      "y": 3
+    }, 
+    // qwerty: Alt_L  
+    "64": { 
+      "hand": "left",
+      "finger": "leftThumb",
+      "x": 2.5,
+      "y": 4
+    }, 
+    // qwerty: space  
+    "65": { 
+      "hand": "left",
+      "finger": "leftThumb",
+      "x": 6.875,
+      "y": 4
+    }, 
+    // qwerty: Caps_Lock  
+    "66": { 
+      "hand": "left",
+      "finger": "leftPinkie",
+      "x": 0.75,
+      "y": 2
+    }, 
+    // qwerty: Alt_R
+    "108": {
+      "hand": "right",
+      "finger": "rightThumb",
+      "x": 10,
+      "y": 4
+    }
+  }
+}
