e3813b902b869d7894b3156726c422ebcefe9517
hbaughman <hbaughman741@gmail.com>
2015-01-24 22:10:48 -0500

    Test + code for buildCurrentStroke

--- business/analyzer.js
+++ business/analyzer.js
@@ -1,3 +1,9 @@
 var dl = {
-
+  buildCurrentStroke: function(character, characters) {
+    var strokeKeycodes =  characters[character],
+        currentStrokes =  _.map(strokeKeycodes, function(keycode) {
+                            return xm.config.keyboard[keycode];
+                          });
+    return currentStrokes;
+  }
 };
--- business/qunit/analyzerTests.js
+++ business/qunit/analyzerTests.js
@@ -1,3 +1,30 @@
 dl.testData = {
-  
+  characters: {
+    "q": ["24"],
+    "Q": ["24", "62"],
+    "!": ["24", "108"],
+    "j": ["44"],
+    "J": ["44", "50"],
+    "(": ["44", "108"],
+    ")": ["44", "108", "50"],
+    ";": ["47"],
+    ":": ["47", "50"]
+  }
 }
+
+// FIXME: This test relies on some of the same logic as what it is testing.
+QUnit.test("dl.buildCurrentStroke", function(assert) {
+  function buildCurrentStrokeTest(character) {
+    var expect =  _.map(dl.testData.characters[character], function(keycode) {
+                    return xm.config.keyboard[keycode];
+                  });
+
+    assert.propEqual(dl.buildCurrentStroke(character, dl.testData.characters), expect);
+  }
+
+  buildCurrentStrokeTest("q");
+  buildCurrentStrokeTest("Q");
+  buildCurrentStrokeTest("(");
+  buildCurrentStrokeTest(")");
+  buildCurrentStrokeTest(":");
+});
